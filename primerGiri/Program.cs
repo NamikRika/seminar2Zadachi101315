68036e7f3a3ec813bcfb917137bce117d726fbfe
NamikR <uchitelnamik@mail.ru>
2022-09-29 11:20:35 +0400

    programma samaya tyajelaya girya

--- primerGiri/Program.cs
+++ primerGiri/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] massivGir = new int[] { 1, 20, 60, 120, 51, 6, 81, 74, 12}; // Объявляем массив гирь
+int schetchij = 0; // объявили переменную счётчик
+int samayaTyajelayaGirya = 0; // объявили переменную для самой тяжёлой гири
+
+ 
+for (int i = 0; i < massivGir.Length; i++) // "начало цикла"
+{
+
+    if(samayaTyajelayaGirya <  massivGir[i]) // "начало условия"
+    {
+        samayaTyajelayaGirya = massivGir[i];
+    }
+    else
+    {
+        
+    }                                         // "конец условия"
+
+
+}                                            // "конец цикла"
+
+Console.WriteLine(samayaTyajelayaGirya);      // "вывод результата"
